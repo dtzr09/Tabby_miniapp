e6a6bb37a4d707ab9651a863d0e36b684631cece
dtzr09 <derricktay17@gmail.com>
2025-08-05 22:06:06 +0800

    added tooltip for remaining balance and expenses

--- components/currentExpenses/IncomeExpenseCard.tsx
+++ components/currentExpenses/IncomeExpenseCard.tsx
@@ -1,7 +1,6 @@
-import { Avatar, Box, Typography } from "@mui/material";
-import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import { Box, Tooltip, Typography } from "@mui/material";
 import { useTheme } from "../../src/contexts/ThemeContext";
-import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import { InfoOutlineRounded } from "@mui/icons-material";
 
 interface IncomeExpenseCardProps {
   amount: number;
@@ -20,28 +19,12 @@ const IncomeExpenseCard = ({ amount, type }: IncomeExpenseCardProps) => {
         flexDirection: { xs: "column", sm: "row" },
       }}
     >
-      <Avatar
-        sx={{
-          width: 40,
-          height: 40,
-          backgroundColor:
-            type === "income" ? colors.incomeBg : colors.expenseBg,
-          display: { xs: "none", sm: "flex" },
-        }}
-      >
-        {type === "income" ? (
-          <TrendingUpIcon sx={{ color: colors.income, fontSize: 20 }} />
-        ) : (
-          <TrendingDownIcon sx={{ color: colors.expense, fontSize: 20 }} />
-        )}
-      </Avatar>
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
           gap: 0.5,
           textAlign: { xs: "center", sm: "left" },
-          order: { xs: type === "income" ? 2 : 1, sm: 1 },
         }}
       >
         <Typography
@@ -62,20 +45,42 @@ const IncomeExpenseCard = ({ amount, type }: IncomeExpenseCardProps) => {
             maximumFractionDigits: 2,
           })}
         </Typography>
-        <Typography
+        <Box
           sx={{
-            color: colors.textSecondary,
-            fontWeight: 550,
-            fontSize: "0.8rem",
-            lineHeight: "1rem",
-            mb: {
-              xs: 0.5,
-            },
-            order: { xs: 1, sm: 2 },
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 0.5,
+            mb: 1,
           }}
         >
-          {type === "income" ? "Income" : "Expense"}
-        </Typography>
+          <Typography
+            sx={{
+              color: colors.textSecondary,
+              fontWeight: 550,
+              fontSize: "0.8rem",
+              lineHeight: "1rem",
+            }}
+          >
+            {type === "income" ? "Income" : "Expense"}
+          </Typography>
+          {type === "expense" && (
+            <Tooltip
+              title="This is your total expenses for the current month."
+              arrow
+              placement="right"
+            >
+              <InfoOutlineRounded
+                fontSize="small"
+                sx={{
+                  color: colors.textSecondary,
+                  cursor: "pointer",
+                  fontSize: "0.9rem",
+                }}
+              />
+            </Tooltip>
+          )}
+        </Box>
       </Box>
     </Box>
   );
