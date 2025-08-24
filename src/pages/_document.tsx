434351212e1c948715b4a6b11f54b1e7c4ff5eb8
dtzr09 <derricktay17@gmail.com>
2025-08-24 16:01:06 +0800

    test layout

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,27 @@ export default function Document() {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover, interactive-widget=resizes-visual-viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
         />
+        <style>{`
+          /* Prevent keyboard from resizing viewport */
+          html {
+            height: 100%;
+            overflow: hidden;
+          }
+          body {
+            height: 100%;
+            overflow: hidden;
+            position: fixed;
+            width: 100%;
+            top: 0;
+            left: 0;
+          }
+          #__next {
+            height: 100vh;
+            overflow: hidden;
+          }
+        `}</style>
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
         <script src="https://telegram.org/js/telegram-web-app.js" />
       </Head>
